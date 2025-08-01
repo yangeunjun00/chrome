3dc8e7e0c3cac8b03ac3b20012a71a0daa4eeaea
yangeunjun00 <s25027@gsm.hs.kr>
2025-08-01 15:57:05 +0900

    Update weather.js

--- JS/weather.js
+++ JS/weather.js
@@ -1,10 +1,18 @@
+const api_key = "7f5930c86ff992954f1a798a1b1227bc";
 function ongeoOk(position) {
-  console.log(position);
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  console.log(`you live in ${lat},${lon}`);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:first-child");
+      const Weather = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      Weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function ongeoError() {
-  alert("위치정보를 가져올수 없다네~~~~");
+  alert("위치정보를 가져올수 없다네");
 }
 navigator.geolocation.getCurrentPosition(ongeoOk, ongeoError);
